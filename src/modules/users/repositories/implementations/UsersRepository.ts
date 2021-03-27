22d409340e76a060194cdb3f651106720d0b94c3
Victor Rodrigues <victorxrp@gmail.com>
2021-03-27 20:22:38 -0300

    Desafio concluído

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,28 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
-  }
+      const user = await this.repository.findOneOrFail({
+        id: user_id,
+      }, {
+        relations: ["games"]
+      });
+     
+      return user;
+    }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM USERS ORDER BY FIRST_NAME ASC'); 
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+    SELECT * FROM USERS
+    WHERE LOWER(first_name) = LOWER($1) AND
+       LOWER(last_name) = LOWER($2)
+    `,[first_name, last_name]); // Complete usando raw query
   }
 }
